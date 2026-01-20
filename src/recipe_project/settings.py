e9fbe1974b18cb788a6af9ef8df6c7ddf7e8d0a6
AHenry95 <ahenry.dev@gmail.com>
2026-01-20 18:24:26 -0500

    Update settings for deployment

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -21,10 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpevh4xz)3w)6xuq6kv7g&3^xf^)gr-n3&p#%9')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
+if not SECRET_KEY:
+    if DEBUG:
+        SECRET_KEY = 'dev-only-insecure-key'
+    else:
+        raise ValueError('No SECRET_KEY set for production!')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'sheltered-basin-42687-d57135d9635e.herokuapp.com']
 
